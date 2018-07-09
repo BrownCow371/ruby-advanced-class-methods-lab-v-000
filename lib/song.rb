dd72a2272410428e1d31196e23a8b51800f5020f
Jamie Brown <jmbrowncow@gmail.com>
2018-07-09 16:11:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    name = filename.split(/(.mp3|-)/)[2].chop
+    name = filename.split(/(.mp3|-)/)[2].lstrip
     self.create_by_name(name)
   end
 
